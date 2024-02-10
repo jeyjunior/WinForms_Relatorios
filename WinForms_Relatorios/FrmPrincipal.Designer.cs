9f64b6c4c516147ba521de54d2c87d80a9896db7
José Junior <jeyjr.dg@gmail.com>
2024-02-10 16:59:30 -0300

    Update UI

--- WinForms_Relatorios/FrmPrincipal.Designer.cs
+++ WinForms_Relatorios/FrmPrincipal.Designer.cs
@@ -28,35 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnRelatorio = new System.Windows.Forms.Button();
+            this.btnRelatorioSimples = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.dtgPrincipal = new System.Windows.Forms.DataGridView();
+            this.colNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colEmail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgPrincipal)).BeginInit();
             this.SuspendLayout();
             // 
-            // btnRelatorio
+            // btnRelatorioSimples
             // 
-            this.btnRelatorio.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnRelatorio.Location = new System.Drawing.Point(229, 183);
-            this.btnRelatorio.Name = "btnRelatorio";
-            this.btnRelatorio.Size = new System.Drawing.Size(143, 46);
-            this.btnRelatorio.TabIndex = 0;
-            this.btnRelatorio.Text = "Relatório";
-            this.btnRelatorio.UseVisualStyleBackColor = true;
-            this.btnRelatorio.Click += new System.EventHandler(this.btnRelatorio_Click);
+            this.btnRelatorioSimples.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRelatorioSimples.Location = new System.Drawing.Point(12, 243);
+            this.btnRelatorioSimples.Name = "btnRelatorioSimples";
+            this.btnRelatorioSimples.Size = new System.Drawing.Size(121, 46);
+            this.btnRelatorioSimples.TabIndex = 0;
+            this.btnRelatorioSimples.Text = "Relatório Simples";
+            this.btnRelatorioSimples.UseVisualStyleBackColor = true;
+            this.btnRelatorioSimples.Click += new System.EventHandler(this.btnRelatorio_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(12, 181);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(270, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(12, 207);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(270, 20);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRegistrar.Location = new System.Drawing.Point(288, 181);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(84, 46);
+            this.btnRegistrar.TabIndex = 3;
+            this.btnRegistrar.Text = "ADD";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // dtgPrincipal
+            // 
+            this.dtgPrincipal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgPrincipal.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colNome,
+            this.colEmail});
+            this.dtgPrincipal.Location = new System.Drawing.Point(12, 12);
+            this.dtgPrincipal.Name = "dtgPrincipal";
+            this.dtgPrincipal.Size = new System.Drawing.Size(360, 150);
+            this.dtgPrincipal.TabIndex = 4;
+            // 
+            // colNome
+            // 
+            this.colNome.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colNome.DataPropertyName = "nome";
+            this.colNome.HeaderText = "Nome";
+            this.colNome.Name = "colNome";
+            this.colNome.ReadOnly = true;
+            // 
+            // colEmail
+            // 
+            this.colEmail.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colEmail.DataPropertyName = "email";
+            this.colEmail.HeaderText = "Email";
+            this.colEmail.Name = "colEmail";
+            this.colEmail.ReadOnly = true;
             // 
             // FrmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 241);
-            this.Controls.Add(this.btnRelatorio);
+            this.ClientSize = new System.Drawing.Size(384, 301);
+            this.Controls.Add(this.dtgPrincipal);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.btnRelatorioSimples);
             this.Name = "FrmPrincipal";
             this.Text = "Principal";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgPrincipal)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnRelatorio;
+        private System.Windows.Forms.Button btnRelatorioSimples;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.DataGridView dtgPrincipal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colEmail;
     }
 }
 
--- WinForms_Relatorios/FrmPrincipal.cs
+++ WinForms_Relatorios/FrmPrincipal.cs
@@ -10,11 +10,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WinForms_Relatorios.Model;
 
 namespace WinForms_Relatorios
 {
     public partial class FrmPrincipal : Form
     {
+        private List<Registro> registros = new List<Registro>();
+
         public FrmPrincipal()
         {
             InitializeComponent();
@@ -42,6 +45,21 @@ namespace WinForms_Relatorios
 
                 File.WriteAllBytes(@"Relatorios\Relatorio_NomeEmail.PDF", mStream.ToArray());
             }
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNome.Text) || string.IsNullOrEmpty(txtEmail.Text))
+                return;
+
+            registros.Add(new Registro { Nome = txtNome.Text.Trim(), Email = txtEmail.Text.Trim() });
+
+            dtgPrincipal.DataSource = registros.Select(i => new
+            {
+                Nome = i.Nome,
+                Email = i.Email,
+            })
+                .ToList();
         }
     }
 }
